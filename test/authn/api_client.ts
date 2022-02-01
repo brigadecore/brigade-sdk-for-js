925f9f42d6d3d1aeea54975aa56184486038f907
Kent Rancourt <kent.rancourt@microsoft.com>
2022-02-01 14:58:07 -0500

    add support for the new whoami endpoint
    Signed-off-by: Kent Rancourt <kent.rancourt@microsoft.com>

--- test/authn/api_client.ts
+++ test/authn/api_client.ts
@@ -1,15 +1,34 @@
 import { APIClient } from "../../src/authn/api_client"
+import { PrincipalTypeUser } from "../../src/authz/role_assignments"
 
 import * as common from "../common"
 
 import "mocha"
 import { assert } from "chai"
+import { PrincipalReference } from "../../src/lib/authz"
 
 describe("api_client", () => {
 
   describe("APIClient", () => {
 
     const client = new APIClient(common.testAPIAddress, common.testAPIToken)
+
+    describe("#whoami", () => {
+      it("should send/receive properly over HTTP", async () => { 
+        const testRef: PrincipalReference = {
+          type: PrincipalTypeUser,
+          id: "stark"
+        }
+        await common.testClient({
+          expectedRequestMethod: "GET",
+          expectedRequestPath: "/v2/whoami",
+          mockResponseBody: testRef,
+          clientInvocationLogic: () => {
+            return client.whoAmI()
+          }
+        })
+      })
+    })
 
     describe("#serviceAccounts", () => {
       it("should return a service accounts client", () => {
